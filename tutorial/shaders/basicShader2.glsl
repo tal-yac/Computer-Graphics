ca4abaa0a098a18bd5fb95158848708a40cf2e7b
Tal <talycb8@gmail.com>
2022-05-27 16:58:03 +0300

    updated palette

--- tutorial/shaders/basicShader2.glsl
+++ tutorial/shaders/basicShader2.glsl
@@ -11,21 +11,21 @@ in vec3 position0;
 float color_range = 0.5;
 vec3 threshold = vec3(0.00001, 0.00001, 0.00001);
 
-vec3 palette[6] = {vec3(1.0, 0, 0),         vec3(0, 1.0, 0),
-                   vec3(0, 0, 1.0),         vec3(1.0, 1.0, 0),
-                   vec3(0.936, 0.5, 0.025), vec3(1.0, 1.0, 1.0)};
+vec3 palette[6] = {vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0),
+                   vec3(0.0, 0.0, 1.0), vec3(1.0, 1.0, 0.0),
+                   vec3(1.0, 0.5, 0.0), vec3(1.0, 1.0, 1.0)};
 
 bool less_than_vec(vec3 v, vec3 u) {
   return v.x < u.x && v.y < u.y && v.z < u.z;
 }
 
 int vec_hash(vec3 v) {
-  return less_than_vec(vec3(0.9, 0.5, 0.5) - v, threshold)  ? 0 :
-		 less_than_vec(vec3(0.5, 0.9, 0.5) - v, threshold)  ? 1 :
-		 less_than_vec(vec3(0.5, 0.5, 0.5) - v, threshold)  ? 2 :
-		 less_than_vec(vec3(0.5, 0.05, 0.5) - v, threshold) ? 3 :
-		 less_than_vec(vec3(0, 0.5, 0.5) - v, threshold)    ? 4 :
-		 													  5 ;
+  return less_than_vec(vec3(0.9, 0.5, 0.5) - v, threshold)    ? 0
+         : less_than_vec(vec3(0.5, 0.9, 0.5) - v, threshold)  ? 1
+         : less_than_vec(vec3(0.5, 0.5, 0.5) - v, threshold)  ? 2
+         : less_than_vec(vec3(0.5, 0.05, 0.5) - v, threshold) ? 3
+         : less_than_vec(vec3(0, 0.5, 0.5) - v, threshold)    ? 4
+                                                              : 5;
 }
 
 out vec4 Color;
